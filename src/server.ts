0f25132633b3a542bc24b64f939675ca94457245
Oleksandr <d43z61p0@gmail.com>
2024-10-05 18:57:50 +0300

    fix bugs

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import contactRoutes from './routes/contactRoutes';
+// import contactRoutes from './routes/contactRoutes';
 // import { json } from 'body-parser';
 import db from './config/db';
 
 dotenv.config();
 
-const app = express();
+// const app = express();
 
-app.use(express.json())
-app.use('/api/contacts', contactRoutes);
+// app.use(express.json())
+// app.use('/api/contacts', contactRoutes);
 
 
 db();
